8a931372d0f14e95dfcca1e1783c32d54f3f6ff1
xohd685 <xohd685@444150AC224.corp.log.intra.laposte.fr>
2021-12-15 15:19:33 +0100

    technical errors serializable

--- src/main/java/com/integrationsi/hrapi/commit/TechnicalCommitError.java
+++ src/main/java/com/integrationsi/hrapi/commit/TechnicalCommitError.java
@@ -1,13 +1,32 @@
 package com.integrationsi.hrapi.commit;
 
 public class TechnicalCommitError {
+	
+	public TechnicalCommitError() {
+		
+	}
+
+			
 	public TechnicalCommitError(TechnicalError error, String content) {
 		super();
-		this.error = error;
+		this.error = error.toString();
 		this.content = content;
 	}
-	TechnicalError error;
-	String content;
+	private String error;
+	private String content;
+	
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
 	
 
 }
--- src/main/java/com/integrationsi/hrapi/commit/TechnicalError.java
+++ src/main/java/com/integrationsi/hrapi/commit/TechnicalError.java
@@ -10,15 +10,39 @@ public enum TechnicalError {
 	BAD_DATA_FORMAT("05", "Problème format de données", "Les données mise à jour ne sont pas au bon format."),
 	DELETE_ERROR("06", "Problème lors de la suppression de données", "Des données ne peuvent pas être suprrimées.");	
 	
-    String code;
-    String label;
-    String description;
+    private String code;
+    private String label;
+    private String description;
     
 	TechnicalError(String code, String label, String description) {
 		this.code = code;
         this.label = label;
         this.description = description;
     }
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 
 }
